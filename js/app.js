816d575899501a4c88c1fff70e6721af7a765344
Ankan <77142111+itzankan-in@users.noreply.github.com>
2023-03-10 16:44:04 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+ let heart= setInterval(() => {
+    let imgArr= Array.from(document.getElementsByClassName("c"))
+    if(imgArr.length != 50) {
+        // console.log(imgArr.length)
+     let img = document.createElement("img");
+     img.src = './img/hart.png'
+     img.setAttribute("class", "c")
+     let rint = (Math.floor(Math.random()* 100))
+     let rint2 = (Math.floor(Math.random()* 100) + 10)
+     let rint3 = (Math.floor(Math.random()* 40)  + 1)
+     let r4 = Math.floor(Math.random()* 2)
+    //  console.log(r4)
+     img.style.left = rint+"%"
+     img.style.height = rint2+"px"
+     img.style.width = rint2+"px"
+    //  r4 === 0? img.style.rotare = -`${rint3}deg` : img.style.rotare = `-${rint3}deg`;
+     document.body.appendChild(img)
+    }
+ }, 500);
+// setTimeout(() => {
+//     clearInterval("heart")
+// }, 5000);
+ function whoose() {
+   
+
+    let box = document.querySelector(".box-cover")
+    box.classList.add("huv")
+    box.style.cursor = "default"
+    let all = document.querySelector(".all")
+    setTimeout(() => {
+        all.removeChild(box)
+    }, 3000);
+
+
+}
+// let audio = new Audio()
+audio.src = "../img/sexysong.mp3"
+audio.play()
